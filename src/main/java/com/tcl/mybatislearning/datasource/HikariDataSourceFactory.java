865de8ea0ff4929673ef36950403b26abe8d8a50
hundanLi <zulongli@qq.com>
2020-08-12 11:11:44 +0800

    HikariCP-2

--- src/main/java/com/tcl/mybatislearning/datasource/HikariDataSourceFactory.java
+++ src/main/java/com/tcl/mybatislearning/datasource/HikariDataSourceFactory.java
@@ -3,6 +3,11 @@ package com.tcl.mybatislearning.datasource;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.datasource.unpooled.UnpooledDataSourceFactory;
+import org.apache.ibatis.io.Resources;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * @author li
@@ -10,8 +15,11 @@ import org.apache.ibatis.datasource.unpooled.UnpooledDataSourceFactory;
  * @date 2020/8/12 10:19
  */
 public class HikariDataSourceFactory extends UnpooledDataSourceFactory {
-    public HikariDataSourceFactory() {
-//        HikariConfig hikariConfig = new HikariConfig("D:\\developer\\java\\JetBrains\\IdeaProjects\\mybatis-learning\\mybatis-learning\\src\\main\\resources\\hikariCP.properties");
-        this.dataSource = new HikariDataSource();
+    public HikariDataSourceFactory() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("hikariCP.properties");
+        Properties props = new Properties();
+        props.load(inputStream);
+        HikariConfig hikariConfig = new HikariConfig(props);
+        this.dataSource = new HikariDataSource(hikariConfig);
     }
 }
